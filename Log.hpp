5efaa9da6e50f2eb70bc73acd2f2de83b0210e2c
qlz <415258758@qq.com>
2023-07-03 15:45:40 +0800

    日志

--- Log.hpp
+++ Log.hpp
@@ -1,3 +1,33 @@
 #pragma once
 
-//1 创建监听套接字失败
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <unistd.h>
+
+enum
+{
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL
+};
+const char *const levalStr[] = {
+    "DEBUG",
+    "INFO",
+    "WARING",
+    "ERROR",
+    "FATAL"};
+
+
+// #define LOG(leval, message) Log(levalStr[leval], message, __FILE__, __LINE__)
+#define LOG(leval, message) Log(#leval, message, __FILE__, __LINE__)
+
+void Log(std::string leval, std::string message, std::string file_name, int line)
+{
+    std::cout << "[" << leval << "]"
+              << "[" << time(nullptr) << "]"
+              << "[" << message << "]"
+              << "[" << file_name << "___" << line << "]" << std::endl;
+}
